1b4771416da37af94d5e1edad27500b5ca862039
kly001 <karenly1961@gmail.com>
2019-06-27 00:37:55 -0400

    Made Corrections to migrations

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: `lambda.db3`
     },
     useNullAsDefault: true,
   },
